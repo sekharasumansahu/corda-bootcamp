ca13b534fd5c4798e33a9f11dd433b62cc504e81
sekharasumansahu <sekharsahu143@gmail.com>
2020-03-20 18:41:30 +0530

    Initial Commit

---
+++ src/main/java/bootcamp/TokenContract.java
@@ -0,0 +1,24 @@
+package bootcamp;
+
+import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.Contract;
+import net.corda.core.transactions.LedgerTransaction;
+import org.jetbrains.annotations.NotNull;
+
+/* Our contract, governing how our state will evolve over time.
+ * See src/main/java/examples/ArtContract.java for an example. */
+public class TokenContract implements Contract {
+    public static String ID = "bootcamp.TokenContract";
+
+    @Override
+    public void verify(@NotNull LedgerTransaction tx) throws IllegalArgumentException {
+
+    }
+
+
+    public interface Commands extends CommandData {
+        class Issue implements Commands { }
+        class Transfer implements Commands { }
+        class Lost implements Commands { }
+    }
+}
